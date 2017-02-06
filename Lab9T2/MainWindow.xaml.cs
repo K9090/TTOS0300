4e102377462ebf391618d2f8a11d886c7ac85eed
K9090 <K9090@student.jamk.fi>
2017-02-06 14:12:06 +0200

    Lab9T2 started

---
+++ Lab9T2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab9T2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        double markkaEx;
+        double input;
+        double output;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            markkaEx = 5.94;
+            output = input / markkaEx;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "€: " + input;
+            textBox1.Text = "Mk: " + output;
+        }
+    }
+}
