8cafd11fc379ff9841978e4615845e2844cc1a1f
K9090 <K9090@student.jamk.fi>
2017-02-13 13:56:19 +0200

    Added demos. Need to continue Wpf_Demo_X3...

---
+++ Wpf_Demo_X3/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.ICT;
+
+namespace Wpf_Demo_X3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // koska useampi metodi (tapahtumankäsittelijä) tulee käyttämään näitä muuttujia
+        // määritellään luokan tasolle luokan jäsenmuuttujiksi
+        JAMK.ICT.HockeyLeague liiga;
+        ObservableCollection<JAMK.ICT.HockeyTeam> joukkueet;
+        int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitMyStuff();
+        }
+        private void InitMyStuff()
+        {
+            // tänne kootusti omien controllien alustukset
+            List<string> muuvit = new List<string>();
+            muuvit.Add("Halloween");
+            muuvit.Add("Star Wars");
+            muuvit.Add("Star Trek");
+            muuvit.Add("Deadpool");
+            muuvit.Add("Stargate");
+            cmbMovies.ItemsSource = muuvit;
+
+            // haetaan SM-Liiga -joukkueet
+            liiga = new JAMK.ICT.HockeyLeague();
+            joukkueet = liiga.GetTeams();
+            cmbTeams.ItemsSource = joukkueet;
+        }
+
+        private void btnBind_Click(object sender, RoutedEventArgs e)
+        {
+            // määritellään Stack -paneelin DataContext
+            // Demo1: DataContexti on olio
+            //HockeyTeam tiimi = new HockeyTeam("KeuPa", "Keuruu");
+            //spRight.DataContext = tiimi;
+            // Demo2: kytketään olio-kokoelman 1. olioon
+            spRight.DataContext = joukkueet[counter];
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (counter < joukkueet.Count - 1)
+                {
+                    counter++;
+                    spRight.DataContext = joukkueet[counter];
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void btnBackward_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (counter > 0)
+                {
+                    counter--;
+                    spRight.DataContext = joukkueet[counter];
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
