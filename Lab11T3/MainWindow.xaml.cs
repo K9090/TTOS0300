8685a8703820b39eec1708828eaae68a413f972b
K9090 <K9090@student.jamk.fi>
2017-02-15 13:11:51 +0200

    Lab11T3 started

---
+++ Lab11T3/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.IT
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Worker> workerList;
+        Worker worker1;
+
+        public void InitMyStuff()
+        {
+            workerList = new List<Worker>();
+
+            workerList.Add(new Worker { SocialID = "121256-664G", FirstName = "Taneli", LastName = "Tampio", DateOfBirth = 12.12.1958 });
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitMyStuff();
+        }
+    }
+}
---
+++ Lab11T3/Person.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    abstract class Person
+    {
+        public string SocialID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public int CalcAge(int age)
+        {
+            age = DateTime.Today.Year - DateOfBirth.Year;
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+
+    class Worker : Person
+    {
+        public string Title { get; set; }
+        public int WorkerID { get; set; }
+        public DateTime StartingDate { get; set; }
+        public float Wage { get; set; }
+
+        public Worker()
+        {
+
+        }
+
+        virtual public float CalcWage()
+        {
+            return Wage;
+        }
+
+        public override string ToString()
+        {
+            return WorkerID + " = " + base.ToString() + ", " + Title;
+        }
+    }
+
+    class FullTime : Worker
+    {
+        public float Salary { get; set; }
+
+        public override float CalcWage()
+        {
+            Wage = Salary;
+            return Wage;
+        }
+    }
+
+    class PartTime : Worker
+    {
+        public float HourPay { get; set; }
+        public int WorkedHours { get; set; }
+
+        public override float CalcWage()
+        {
+            Wage = HourPay * WorkedHours;
+            return Wage;
+        }
+    }
+}
