070d200f5b8ace9f52f1ea50c2efcbc67f218909
K9090 <K9090@student.jamk.fi>
2017-02-20 13:15:56 +0200

    Lab12Demo1 ok

---
+++ Lab12Demo1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVMDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MVVMDemo.ViewModel.StudentViewModel svmo = new ViewModel.StudentViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            svmo.LoadStudents();
+        }
+
+        private void StudentViewControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            StudentViewControl.DataContext = svmo;
+        }
+
+        private void dgStudents_Loaded(object sender, RoutedEventArgs e)
+        {
+            dgStudents.DataContext = svmo.Students;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Add to observable collection a new object
+            MVVMDemo.Model.Student fresh = new Model.Student();
+            fresh.FirstName = txtFirstName.Text;
+            fresh.LastName = txtLastName.Text;
+            fresh.AsioID = txtAsioID.Text;
+            svmo.Students.Add(fresh);
+            // Clear textboxes
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtAsioID.Text = "";
+        }
+    }
+}
---
+++ Lab12Demo1/ViewModel/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using MVVMDemo.Model;
+using System.Collections.ObjectModel;
+
+namespace MVVMDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Mark", LastName = "Allen", AsioID = "A4324" });
+            students.Add(new Student { FirstName = "John", LastName = "Doe", AsioID = "R4365" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Kernell", AsioID = "J5699" });
+            Students = students;
+        }
+    }
+}
