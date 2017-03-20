7ea2ccf364ea86a01f6a0d855ebc26644c789621
K9090 <K9090@student.jamk.fi>
2017-03-20 13:51:29 +0200

    Add two demos

---
+++ WPF_VR_Trains/VMTrains.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT
+{
+    public class TrainsVM
+    {
+        public static List<Train> GetTrainsAt(string station)
+        {
+            try
+            {
+                List<Train> trains = new List<Train>();
+                if (station == "testi" || station == "")
+                {
+                    //Vaihe1: Placebo palauttaa oikean muotoista dataa
+                    //keksitään muutama juna
+                    Train tr = new Train();
+                    tr.TrainNumber = "666";
+                    tr.DepDate = new DateTime(2017, 3, 21);
+                    tr.TargetStation = "Highway to Hell";
+                    trains.Add(tr);
+                }
+                else
+                {
+                    //muutetaan haettu json olio-kokoelmaksi
+                    string tmp = JAMK.IT.API.GetJsonFromLiikenneVirasto(station);
+                    trains = JsonConvert.DeserializeObject<List<Train>>(tmp);
+                }
+                //palautus
+                return trains;
+            }
+            
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
